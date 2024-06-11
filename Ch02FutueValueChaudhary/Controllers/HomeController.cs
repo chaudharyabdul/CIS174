45f63ecbbecc819d7510fc9fb9401f4feae17ace
Abdul Chaudhary <s.abdulchaudhary@hotmail.com>
2024-06-10 19:22:00 -0500

    Create FutureValue
    New Project

---
+++ Ch02FutueValueChaudhary/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Ch02FutueValueChaudhary.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Ch02FutueValueChaudhary.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.FV = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(FutureValueModel model)
+        {
+            ViewBag.FV = model.CalculateFutureValue();
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Ch02FutueValueChaudhary/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ch02FutueValueChaudhary.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
