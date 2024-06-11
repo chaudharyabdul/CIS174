ed43ee5677d2ee60379e14e4507cd67e30134618
Abdul Chaudhary <s.abdulchaudhary@hotmail.com>
2024-06-10 19:17:30 -0500

    New Projects
    Uploaded previous apps under one repo CIS174

---
+++ FirstResponsiveWebAppChaudhary/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using FirstResponsiveWebAppChaudhary.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstResponsiveWebAppChaudhary.Controllers
+{
+    public class HomeController : Controller
+    {
+        private const int CurrentYear = 2024; // Global constant
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Message = $"Hello, {user.Name}! On December 31st, {CurrentYear}, you will be {user.AgeThisYear(CurrentYear)} years old.";
+
+                if (user.BirthDate.HasValue)
+                {
+                    ViewBag.ExtraMessage = $"Today, you are {user.AgeToday()} years old."; // If DOB provided
+                }
+            }
+            return View(user);
+        }
+    }
+}
---
+++ FirstResponsiveWebAppChaudhary/Models/User.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FirstResponsiveWebAppChaudhary.Models
+{
+    public class User
+    {
+        // Constant for minimum year allowed
+        private const int MinimumBirthYear = 1900;
+
+        [Required(ErrorMessage = "Please enter your name.")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your birth year.")]
+        [Range(MinimumBirthYear, int.MaxValue, ErrorMessage = "Invalid birth year.")]
+        public int BirthYear { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? BirthDate { get; set; } // Nullable
+
+        // Current year age method 
+        public int AgeThisYear(int currentYear)
+        {
+            return currentYear - BirthYear;
+        }
+
+        // Current age based on DOB method
+        public int AgeToday()
+        {
+            if (BirthDate.HasValue)
+            {
+                var today = DateTime.Today;
+                var age = today.Year - BirthDate.Value.Year;
+                if (BirthDate.Value.Date > today.AddYears(-age)) // Adjust if DOB hasn't occured this year
+                {
+                    age--;
+                }
+                return age;
+            }
+            return 0; // Default for missing DOB
+        }
+    }
+}
