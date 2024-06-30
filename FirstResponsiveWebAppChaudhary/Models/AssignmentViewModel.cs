c92c7a1b5e979b97621fa1f00dab46555abd8eee
Abdul Chaudhary <s.abdulchaudhary@hotmail.com>
2024-06-30 10:10:49 -0500

    Updated for Assignment6.1 Task
    Added AssignmentController.cs, AssignmentViewModel.cs, Student.cs, and Assignment\Index.cshtml
    Updated Program.cs and Shared\_Layout.cs

---
+++ FirstResponsiveWebAppChaudhary/Models/AssignmentViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FirstResponsiveWebAppChaudhary.Models
+{
+    public class AssignmentViewModel
+    {
+        public List<Student> Students { get; set; }
+        public int AccessLevel { get; set; }
+    }
+}
