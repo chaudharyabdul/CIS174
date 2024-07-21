c4b419d7eac1c7816288f8fa04ed6473aead3c56
Abdul Chaudhary <s.abdulchaudhary@hotmail.com>
2024-07-21 13:10:27 -0500

    Added Tag Helper & View Components

---
+++ Olympics/TagHelpers/SubmitButtonTagHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Olympics.TagHelpers
+{
+    [HtmlTargetElement("submit-button")]
+    public class SubmitButtonTagHelper : TagHelper
+    {
+        public string Text { get; set; } = "Submit";
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "button";
+            output.Attributes.SetAttribute("type", "submit");
+            output.Attributes.SetAttribute("class", "btn btn-primary");
+            output.Content.SetContent(Text);
+        }
+    }
+}
---
+++ Olympics/ViewComponents/StatusViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Olympics.ViewComponents
+{
+    public class StatusViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(string status)
+        {
+            return View("Default", status);
+        }
+    }
+}
